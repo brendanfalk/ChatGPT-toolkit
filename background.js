116a18bd5ec6c287e06639fc4d5b25dcc9232a83
Brendan Falk <brendan@fig.io>
2023-04-15 13:52:51 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      const url = new URL(details.url);
+      if (url.hostname === "chat.openai.com") {
+        url.searchParams.set("model", "gpt-4");
+        return { redirectUrl: url.href };
+      }
+    },
+    { urls: ["https://chat.openai.com"] },
+    // ["blocking"]
+  );
+  
